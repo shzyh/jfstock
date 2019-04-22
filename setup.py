4e006249b75ef958584a12c03fd22f9f457f250b
jiangfeng <yxwmjf@gmail.com>
2019-04-22 16:38:14 +0800

    add file setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+atr ={
+	"name":"jfstock",
+	"version":"1.0",
+	"description":"get stock data from database or calc data",
+	"url":"https://github.com/shzyh/jfstock",
+	"author_email":"yxwmjf@hotmail.com",
+	"author":"jf",
+	"license":"MIT",
+	"packages":["jfstock"],
+	"zip_safe":False
+}
+
+setup(**atr)
